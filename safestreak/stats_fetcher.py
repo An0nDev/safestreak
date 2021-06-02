ffc914f2373f35cd3a3dfc1ddba149e44d3f4bc7
Eric Reed <mrericcoolnessreed@gmail.com>
2021-06-02 00:58:59 -0400

    fixed bad fkdr calc

--- safestreak/stats_fetcher.py
+++ safestreak/stats_fetcher.py
@@ -25,7 +25,7 @@ class StatsFetcher:
         if not success: raise Exception (f"couldn't get stats: {og_stats}")
         star = xp_to_level (xp = og_stats ["Experience"] if "Experience" in og_stats else 0)
         final_kills = og_stats ["final_kills_bedwars"] if "final_kills_bedwars" in og_stats else 0
-        final_deaths = og_stats ["final_kills_bedwars"] if "final_kills_bedwars" in og_stats else 0
+        final_deaths = og_stats ["final_deaths_bedwars"] if "final_deaths_bedwars" in og_stats else 0
         stats = {
             "star": star,
             "fkdr": (final_kills / final_deaths) if final_deaths > 0 else 0,
